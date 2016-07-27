7788905432af5f4c138b8814d70bffb351c64baa
David <agile_check_in@Davids-MBP.attlocal.net>
2016-07-27 13:16:36 -0500

    [#00] Add tap gesture

--- stop.py/ViewController.swift
+++ stop.py/ViewController.swift
@@ -8,21 +8,55 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
-    @IBOutlet weak var counter: UITextView!
+    var timerRunning = false
+    var timer = NSTimer()
+    var count = 0
+    var counter = UILabel()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.addSubview(counter)
+        let views = ["counter": counter]
         counter.text = "00:00:00"
+        counter.translatesAutoresizingMaskIntoConstraints = false
+        let lrCons = NSLayoutConstraint.constraintsWithVisualFormat("V:|-[counter]-|", options: [.AlignAllCenterY ], metrics: nil, views: views)
+        self.view.addConstraints(lrCons)
+        let tbCons = NSLayoutConstraint.constraintsWithVisualFormat("H:|-[counter]-|", options: [], metrics: nil, views: views)
+        self.view.addConstraints(tbCons)
         counter.textColor = .whiteColor()
         counter.textAlignment = .Center
-        counter.font = UIFont.systemFontOfSize(50)
+        counter.font = .systemFontOfSize(50)
         counter.backgroundColor = .clearColor()
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(startMyTimer)))
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func startMyTimer() -> Void {
+        if timerRunning {
+            timer.invalidate()
+            timerRunning = false
+        } else{
+            timer = .scheduledTimerWithTimeInterval(1, target: self, selector: #selector(updateClock), userInfo: nil, repeats: true)
+            timerRunning = true
+        }
+        
+    }
+    
+    func updateClock() -> Void {
+        let (hours, minutes, seconds) = secondsBrokenOut()
+        count += 1
+        counter.text = "\(hours):\(minutes):\(seconds)"
+    }
+    
+    func secondsBrokenOut() -> (Int, Int, Int) {
+        return (count / 3600, (count % 3600) / 60, (count % 3600) % 60)
     }
 
 
